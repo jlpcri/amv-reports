9b3d088e9d58161a7686b9ca2f57dda2b8e558e6
Songqing Liu <songqing.liu@amvholdings.com>
2021-09-22 21:43:59 -0500

    Add timezone control to app GURU-3441

--- src/app/shared/data-table/tableDataSource.ts
+++ src/app/shared/data-table/tableDataSource.ts
@@ -1,12 +1,14 @@
-import { DataSource } from '@angular/cdk/collections';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { map } from 'rxjs/operators';
-import { Observable, merge, Subject } from 'rxjs';
+import {DataSource} from '@angular/cdk/collections';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {map} from 'rxjs/operators';
+import {merge, Observable, Subject} from 'rxjs';
 import {ColumnDef} from '../types/columnDef';
-import * as moment from 'moment';
+import * as moment from 'moment-timezone';
 import getPath from 'lodash-es/get';
 import {ExportToCsv} from 'export-to-csv';
+import {Timezone} from '../types/timezone';
+import {GlobalConstants} from '../global-constants';
 
 export class TableDataSource<T> extends DataSource<T> {
     data: T[] = [];
@@ -20,11 +22,17 @@ export class TableDataSource<T> extends DataSource<T> {
     title: string;
     totalRecords: number;
 
+    selectedTimezones$ = new Subject<string>();
+    selectedTimezone = '';
+    timezones: Timezone[];
+
     constructor(columns: ColumnDef[], title: string = 'Data Table') {
         super();
 
         this.columns = columns;
         this.title = title;
+        this.selectedTimezone = moment.tz.guess();
+        this.timezones = GlobalConstants.timezones;
 
         this.data$.subscribe({
             next: (data) => {
@@ -48,6 +56,11 @@ export class TableDataSource<T> extends DataSource<T> {
                 this.displayedColumns = displayedColumns;
             }
         });
+        this.selectedTimezones$.subscribe({
+            next: selectedTimezone => {
+                this.selectedTimezone = selectedTimezone;
+            }
+        });
     }
 
     format(data) {
@@ -56,7 +69,7 @@ export class TableDataSource<T> extends DataSource<T> {
             const newRow = {...row};
             this.columns.forEach(col => {
                 if (col.type === 'date' && row[col.field]?.length > 0) {
-                    newRow[col.field] = moment.utc(row[col.field]).local().format('YYYY-MM-DD');
+                    newRow[col.field] = moment.tz(row[col.field], this.selectedTimezone).format('YYYY-MM-DD');
                 } else if (col.type === 'money') {
                     newRow[col.field] = parseFloat(row[col.field] || 0).toFixed(2);
                 }
